6bc91c497580cdd82fa9d73c61acdeed4b0b862f
rudaruza20 <rdrz2007@hotmail.com>
2023-11-05 15:30:39 -0500

    commit inicial

---
+++ Intro.java
@@ -0,0 +1,12 @@
+public class Intro {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        // Definir y comparar dos variables - con cambio
+        // compose
+    
+    }
+
+    // dsds
+}
