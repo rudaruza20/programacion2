4f55b4c3a29def20b496ddc5e0513dd4b1413b8f
rudaruza20 <rdrz2007@hotmail.com>
2023-11-05 15:56:48 -0500

    first commit

--- Intro.java
+++ Intro.java
@@ -1,5 +1,6 @@
 public class Intro {
-
+    private int var1;
+    private double var2;
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo");
@@ -9,4 +10,11 @@ public class Intro {
     }
 
     // dsds
+    public int getVar1 () {
+        return this.var1;
+    }
+    public double getVar2 (){
+        return this.var2;
+    }
+    
 }
